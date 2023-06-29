07b519cde041919cf88cc62e8871e20666f75589
felix <gathagefelix@gmail.com>
2023-06-29 08:59:38 +0300

    image can be displayed on the post field

--- post/urls.py
+++ post/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from .views import *
 from .views import PostViewSet
 from rest_framework import routers
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
@@ -11,4 +13,5 @@ router.register('post', PostViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-]
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
